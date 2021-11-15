3c8904e0b02c31973d9a5d6debd1342b76c673c1
niskasam <samnisk@outlook.com>
2021-11-15 22:53:53 +0200

    snFindAddress2, 15.11.2021

--- App.js
+++ App.js
@@ -1,13 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useRef} from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Alert, FlatList} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
 export default function App() {
+
+  const key = 'QH7C1sdCLwdhzG0XT0uefsRp72HMXkHf';
+
+  const [region, setRegion] = useState({
+    latitude: 60.451650744313014,
+    longitude: 22.267172535195293,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221
+  });
+
+  const [location, setLocation] = useState('');
+
+
+  const getAddress = () => {
+    
+    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${key}&street=${location}`)
+    .then(response => response.json())
+    .then(mapquestData => {
+      setRegion({
+        ...region,
+        latitude:mapquestData.results[0].locations[0].latLng.lat,
+        longitude:mapquestData.results[0].locations[0].latLng.lng
+      })
+    }).catch(error => {Alert.alert('error', error)})
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <MapView
+       style={styles.map}
+       region={region}
+      >
+        <Marker
+          coordinate={{
+            latitude:region.latitude,
+            longitude:region.longitude
+          }}
+        />
+      
+        </MapView>
+
+    <TextInput
+      onChangeText={text => setLocation(text)}
+      style={styles.input}
+      title="Search"
+      placeholder="Type address and press search"
+      >
+    </TextInput>
+
+    <Button
+    title="Search"
+    onPress={getAddress}
+    ></Button>
     </View>
+
+   
+
+  
   );
 }
 
@@ -18,4 +72,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  map: {
+    flex: 1,
+    width: "100%",
+    height: "100%"
+  },
+
+  input:{
+    width: "100%",
+    textAlign:'center',
+  }
+
 });
